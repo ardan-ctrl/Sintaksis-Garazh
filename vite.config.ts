23d60f3477b438b8c7fe1e9f2a51796e4c7f5db5
ardan-ctrl <eveardann@gmail.com>
2025-10-01 00:14:00 +0100

    Add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Sintaksis-Garazh/',
       server: {
         port: 3000,
         host: '0.0.0.0',
